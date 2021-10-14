44daff997b166eecd164dd95e9b6b44e1c258cff
clopezog <92496164+clopezog@users.noreply.github.com>
2021-10-14 12:25:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,73 @@
 
-<?php require 'header.php'; ?>
+<?php require 'header.php'; <! DOCTYPE html>
+< html  lang = " zxx "  class = " no-js " >
+< cabeza >
+  < meta  name = " viewport "  content = " width = device-width, initial-scale = 1, shrink-to-fit = no " >
+  < link  rel = " icono de acceso directo "  href = " img / fav.png " >
+  < meta  name = " autor "  content = " colorlib " >
+  < meta  name = " description "  content = " " >
+  < meta  name = " palabras clave "  contenido = " " >
+  < meta  charset = " UTF-8 " >
+  < título > TravelAysen </ título >
+  < link  href = " https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700 "  rel = " stylesheet " >
+    <! -
+    CSS
+    =========================================== ->
+    < link  rel = " stylesheet "  href = " css / linearicons.css " >
+    < link  rel = " stylesheet "  href = " css / font-awesome.min.css " >
+    < link  rel = " stylesheet "  href = " css / bootstrap.css " >
+    < link  rel = " stylesheet "  href = " css / magnific-popup.css " >
+    < link  rel = " stylesheet "  href = " css / jquery-ui.css " >
+    < link  rel = " stylesheet "  href = " css / nice-select.css " >
+    < link  rel = " stylesheet "  href = " css / animate.min.css " >
+    < link  rel = " stylesheet "  href = " css / owl.carousel.css " >
+    < link  rel = " stylesheet "  href = " css / main.css " >
+    < link  type = " text / css "  rel = " stylesheet "  href = " css / Whatsapp.css " />
+  </ cabeza >
+  < cuerpo >
+    < header  id = " header " >
+  < div  id = " top-header " >
+  < div  class = " contenedor " >
+    < ul  class = " header-links pull-left " >
+      < Li > < un  href = " https://api.whatsapp.com/send?phone=+56979053267&text=Hola%21tengo%20una%20consulta "  clase = " flotador "  target = " _blank " > < i  clase = " fa fa-whatsapp my-float " > </ i > </ a > </ li >
+    </ ul >
+  </ div >
+</ div >
+      < div  class = " header-top " >
+        < div  class = " contenedor " >
+          < div  class = " row align-items-center " >
+            < div  class = " col-lg-6 col-sm-6 col-6 header-top-left " >
+              < ul >
+              </ ul >
+            </ div >
+            < div  class = " col-lg-6 col-sm-6 col-6 header-top-right " >
+            < div  class = " header-social " >
+              < Un  href = " # " > < i  clase = " fa fa-facebook " > </ i > </ a >
+              < Un  href = " # " > < i  clase = " fa fa-twitter " > </ i > </ a >
+              < Un  href = " # " > < i  clase = " fa fa-instagram " > </ i > </ a >
+            </ div >
+            </ div >
+          </ div >
+        </ div >
+      </ div >
+      < div  class = " menú principal del contenedor " >
+        < div  class = " row align-items-center justify-content-between d-flex " >
+            < div  id = " logo " >
+              < Un  href = " index.php " > < img  src = " img / logo.png "  alt = " "  título = " " /> </ una >
+            </ div >
+            < nav  id = " nav-menu-container " >
+              < ul  class = " nav-menu " >
+                < Li > < un  href = " index.php " > Inicio </ una > </ li >
+                < Li > < un  href = " tours.php " > Tours </ una > </ li >
+                < Li > < un  href = " programas.php " > Programas Carretera Austral </ una > </ li >
+                < Li > < un  href = " trekking.php " > Trekking </ una > </ li >
+                < Li > < un  href = " serviciosEspeciales.php " > Servicios Especiales </ una > </ li >
+                < Li > < un  href = " sobreNosotros.php " > Sobre Nosotros </ una > </ li >
+              </ ul >
+            </ nav > <! - # nav-menu-container ->
+        </ div >
+      </ div >
+</ header > <! - #header ->
 
 
 			<!-- start banner Area -->
